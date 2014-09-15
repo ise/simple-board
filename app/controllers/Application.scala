546da45053c8f8d707ed1237af9bcbc4fbf0487f
mastakeu <isedaisuke@gmail.com>
2014-09-15 12:41:30 +0900

    スレッド用エントリポイント追加

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,9 +2,14 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models._
 
 object Application extends Controller {
 
   def index = TODO
+  def thread(threadId: Long) = Action {
+    val res = Response.findAllByThreadId(threadId)
+    Ok(views.html.index("Your new application is ready."))
+  }
 
 }
